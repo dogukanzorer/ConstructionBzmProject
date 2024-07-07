8d9fc3dd52189794d04695d18e7c2462acfdc29e
dogukanzorer <godneu123@hotmail.com>
2024-07-08 00:27:52 +0300

    git last

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/js/script.js',
+    entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
