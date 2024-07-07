ea3cbd8352105d26cfdcdeae1f66c998dff484e5
dogukanzorer <godneu123@hotmail.com>
2024-07-08 00:12:14 +0300

    last

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/script.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    mode: 'development'
+};
